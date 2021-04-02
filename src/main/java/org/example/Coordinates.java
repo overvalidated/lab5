528a59998c3a64e4702e96c3fa6aff8f0d36d13c
Гость <Guest@iMac-ITMO-9.local>
2021-04-02 19:46:55 +0300

    Added more constructors

--- src/main/java/org/example/Coordinates.java
+++ src/main/java/org/example/Coordinates.java
@@ -7,6 +7,8 @@ public class Coordinates {
     private double y;
 
     public Coordinates(long x, double y) {// throws ParseException 
-
+        if (x < 643) throw new IllegalArgumentException("X must be less than 643");
+        this.x = x;
+        this.y = y;
     }
 }
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -6,4 +6,12 @@ public class Person {
     private Color hairColor; //Поле не может быть null
     private Country nationality; //Поле не может быть null
     private Location location; //Поле не может быть null
+
+    public Person(float height, Color eyeColor, Color hairColor, Country nationality, Location location) {
+        if (height <= 0) throw new IllegalArgumentException("Height must be positive");
+        if (eyeColor == null) throw new IllegalArgumentException("Eye color can't be missing value");
+        if (hairColor == null) throw new IllegalArgumentException("Hair can't be missing value");
+        if (nationality == null) throw new IllegalArgumentException("Nationality can't be missing value");
+        if (location == null) throw new IllegalArgumentException("Location can't be missing value");
+    }
 }

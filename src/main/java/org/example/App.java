1f4bd7afe1c0b8fefe3b9fcab33decc816de0480
Гость <Guest@iMac-ITMO-6.local>
2021-04-02 11:07:55 +0300

    just testing

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,14 +1,8 @@
 package org.example;
-import java.io.PrintWriter;
-import java.io.InputStreamReader;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        String filename = args[0];
-        // загрузка данных в коллекцию из файла
 
-        // определение команды
+public class App {
+    public static void main( String[] args ) {
+        
     }
 }
--- src/main/java/org/example/Coordinates.java
+++ src/main/java/org/example/Coordinates.java
@@ -6,9 +6,7 @@ public class Coordinates {
     private long x; //Максимальное значение поля: 643
     private double y;
 
-    public Coordinates(long x, double y) throws ParseException {
-        if (y > 643) throw new ParseException("y не может быть больше 643");
-        this.x = x;
-        this.y = y;
+    public Coordinates(long x, double y) {// throws ParseException 
+
     }
 }

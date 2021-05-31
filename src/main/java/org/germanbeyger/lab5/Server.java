2f34130b9b32b7f75bcdcba02fb2e950880e131e
German Beiger <germaniga4@gmail.com>
2021-05-31 12:03:26 +0300

    another commit

--- src/main/java/org/germanbeyger/lab5/Server.java
+++ src/main/java/org/germanbeyger/lab5/Server.java
@@ -75,11 +75,11 @@ public class Server {
                                 System.out.printf("Ticket verification: %b\n", command.getTicket().verify());
                             }
                         } catch (ClassNotFoundException ex) {
-                            ex.printStackTrace(); // remove it later
+                            // ex.printStackTrace(); // remove it later
                         } catch (StreamCorruptedException ex) {
-                            ex.printStackTrace();
+                            // ex.printStackTrace();
                         } catch (Exception ex) {
-                            ex.printStackTrace();
+                            // ex.printStackTrace();
                         }
 
                     }

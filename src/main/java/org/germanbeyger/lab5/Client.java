62acbb9b7825c6af4c72060404e51d56e326afde
German Beiger <germaniga4@gmail.com>
2021-05-31 17:40:13 +0300

    another commit

--- src/main/java/org/germanbeyger/lab5/Client.java
+++ src/main/java/org/germanbeyger/lab5/Client.java
@@ -14,37 +14,40 @@ import org.germanbeyger.lab5.server_commands.SendableCommand;
 
 public class Client {
     public static void main(String[] args) {
-        System.out.println("Attempting to connect to server...");
-        try ( Socket socket = new Socket(args[0], Integer.parseInt(args[1]));
-                Scanner stdInScanner = new Scanner(System.in)) {
-            
-            OutputStream out = socket.getOutputStream();
-            System.out.println("Connected!");
-
-            String nextCommand = null;
+        try (Scanner stdInScanner = new Scanner(System.in)) {
             while (true) {
-                nextCommand = stdInScanner.nextLine();
-                String[] commandArgs = nextCommand.split(" ");
-                // commandArgs[0] is a main command
-                // commangdArgs[1] is an argument.
-                SendableCommand command = new SendableCommand(Arrays.copyOfRange(commandArgs, 1, commandArgs.length), commandArgs[0]);
-                ObjectOutputStream objStream = new ObjectOutputStream(out);
-                objStream.writeObject(command);
-                out.flush();
+                try (Socket socket = new Socket(args[0], Integer.parseInt(args[1]))) {
+                    OutputStream out = socket.getOutputStream();
+
+                    String nextCommand = null;
+
+                    nextCommand = stdInScanner.nextLine();
+                    String[] commandArgs = nextCommand.split(" ");
+                    // commandArgs[0] is a main command
+                    // commangdArgs[1] is an argument.
+                    SendableCommand command = new SendableCommand(
+                            Arrays.copyOfRange(commandArgs, 1, commandArgs.length), commandArgs[0]);
+                    ObjectOutputStream objStream = new ObjectOutputStream(out);
+                    objStream.writeObject(command);
+                    System.out.println("Sent!");
+                    out.flush();
+                    out.close();
+
+                    // ObjectOutputStream objStream = new ObjectOutputStream(out);
+                    // objStream.writeObject(command);
+                    // BufferedOutputStream bufStream = new
+                    // BufferedOutputStream(socket.getOutputStream());
+
+                } catch (UnknownHostException ex) {
+                    System.out.println("The hostname is unknown. ");
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    System.out.println("Exception...");
+                }
+
             }
-            // ObjectOutputStream objStream = new ObjectOutputStream(out);
-            // objStream.writeObject(command);
-            // BufferedOutputStream bufStream = new BufferedOutputStream(socket.getOutputStream());
+        } finally {
             
-            
-            
-        } catch (UnknownHostException ex) {
-            System.out.println("The hostname is unknown. ");
-        } 
-        catch (IOException ex) {
-            ex.printStackTrace();
-            System.out.println("Exception...");
-        } 
-       
+        }
     }
 }

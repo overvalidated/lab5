4e2bf988c30c05ec645921f0f8c37e1ec68a0268
German Beiger <germaniga4@gmail.com>
2021-05-31 12:31:26 +0300

    another commit

--- src/main/java/org/germanbeyger/lab5/Server.java
+++ src/main/java/org/germanbeyger/lab5/Server.java
@@ -25,74 +25,108 @@ public class Server {
         System.out.println(PORT);
 
         ServerSocketChannel sChannel;
-        Selector connectionSelector = Selector.open();
+        // Selector connectionSelector = Selector.open();
 
         sChannel = ServerSocketChannel.open();
         sChannel.socket().bind(new InetSocketAddress(PORT));
         sChannel.configureBlocking(false);
-        sChannel.register(connectionSelector, SelectionKey.OP_ACCEPT);
+        // sChannel.register(connectionSelector, SelectionKey.OP_ACCEPT);
 
         while (true) {
-            // blocking until at least one channel is ready
-            int readyChannels = connectionSelector.selectNow();
+            SocketChannel socketChannel =
+            sChannel.accept();
 
-            if(readyChannels == 0) continue;
-
-            Set<SelectionKey> selectedKeys = connectionSelector.selectedKeys(); 
-            Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
-             
-            // new tcp connection
-            while(keyIterator.hasNext()) {
-                SelectionKey key = keyIterator.next();
-                keyIterator.remove();
-
-                if (key.isAcceptable()) {
-                    ServerSocketChannel serverChannel = (ServerSocketChannel) key.channel();
-                    SocketChannel socketChannel = serverChannel.accept();
-                    
-                    if (socketChannel != null){
-                        socketChannel.configureBlocking(false);
-                        socketChannel.register(connectionSelector, SelectionKey.OP_READ);
-                    }
+            if(socketChannel != null) {
+                ByteBuffer buffer = ByteBuffer.allocate(8192);
+                int bytesRead = socketChannel.read(buffer);
+                while (bytesRead != -1) {
+                    bytesRead = socketChannel.read(buffer);
                 }
-
-                if (key.isReadable()) {
-                    try {
-                        SocketChannel socketChannel = (SocketChannel) key.channel();
-                        ByteBuffer buffer = ByteBuffer.allocate(8192);
-                        int bytesRead = socketChannel.read(buffer);
-                        while (bytesRead != -1) {
-                            bytesRead = socketChannel.read(buffer);
-                        }
-                        if (buffer.position() == 0) continue;
-                        try {
-                            ByteArrayInputStream bytesStream = new ByteArrayInputStream(buffer.array());
-                            ObjectInputStream objStream = new ObjectInputStream(bytesStream);
-                            Object deserialized = objStream.readObject();
-                            if (deserialized instanceof SendableCommand) {
-                                SendableCommand command = (SendableCommand) deserialized;
-                                System.out.println(command);
-                                System.out.printf("Ticket verification: %b\n", command.getTicket().verify());
-                                // System.exit(-1);
-                            }
-                        } catch (ClassNotFoundException ex) {
-                            ex.printStackTrace(); // remove it later
-                            // System.exit(-1);
-                        } catch (StreamCorruptedException ex) {
-                            ex.printStackTrace();
-                            // System.exit(-1);
-                        } catch (Exception ex) {
-                            ex.printStackTrace();
-                            // System.exit(-1);
-                        }
-
-                    }
-                    catch (ClassCastException ex) {
-                        ex.printStackTrace(); // remove it later
+                if (buffer.position() == 0) continue;
+                try {
+                    ByteArrayInputStream bytesStream = new ByteArrayInputStream(buffer.array());
+                    ObjectInputStream objStream = new ObjectInputStream(bytesStream);
+                    Object deserialized = objStream.readObject();
+                    if (deserialized instanceof SendableCommand) {
+                        SendableCommand command = (SendableCommand) deserialized;
+                        System.out.println(command);
+                        System.out.printf("Ticket verification: %b\n", command.getTicket().verify());
                         // System.exit(-1);
                     }
+                } catch (ClassNotFoundException ex) {
+                    ex.printStackTrace(); // remove it later
+                    // System.exit(-1);
+                } catch (StreamCorruptedException ex) {
+                    ex.printStackTrace();
+                    // System.exit(-1);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                    // System.exit(-1);
                 }
             }
         }
+
+        // while (true) {
+        //     // blocking until at least one channel is ready
+        //     int readyChannels = connectionSelector.selectNow();
+
+        //     if(readyChannels == 0) continue;
+
+        //     Set<SelectionKey> selectedKeys = connectionSelector.selectedKeys(); 
+        //     Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
+             
+        //     // new tcp connection
+        //     while(keyIterator.hasNext()) {
+        //         SelectionKey key = keyIterator.next();
+        //         keyIterator.remove();
+
+        //         if (key.isAcceptable()) {
+        //             ServerSocketChannel serverChannel = (ServerSocketChannel) key.channel();
+        //             SocketChannel socketChannel = serverChannel.accept();
+                    
+        //             if (socketChannel != null){
+        //                 socketChannel.configureBlocking(false);
+        //                 socketChannel.register(connectionSelector, SelectionKey.OP_READ);
+        //             }
+        //         }
+
+        //         if (key.isReadable()) {
+        //             try {
+        //                 SocketChannel socketChannel = (SocketChannel) key.channel();
+        //                 ByteBuffer buffer = ByteBuffer.allocate(8192);
+        //                 int bytesRead = socketChannel.read(buffer);
+        //                 while (bytesRead != -1) {
+        //                     bytesRead = socketChannel.read(buffer);
+        //                 }
+        //                 if (buffer.position() == 0) continue;
+        //                 try {
+        //                     ByteArrayInputStream bytesStream = new ByteArrayInputStream(buffer.array());
+        //                     ObjectInputStream objStream = new ObjectInputStream(bytesStream);
+        //                     Object deserialized = objStream.readObject();
+        //                     if (deserialized instanceof SendableCommand) {
+        //                         SendableCommand command = (SendableCommand) deserialized;
+        //                         System.out.println(command);
+        //                         System.out.printf("Ticket verification: %b\n", command.getTicket().verify());
+        //                         // System.exit(-1);
+        //                     }
+        //                 } catch (ClassNotFoundException ex) {
+        //                     ex.printStackTrace(); // remove it later
+        //                     // System.exit(-1);
+        //                 } catch (StreamCorruptedException ex) {
+        //                     ex.printStackTrace();
+        //                     // System.exit(-1);
+        //                 } catch (Exception ex) {
+        //                     ex.printStackTrace();
+        //                     // System.exit(-1);
+        //                 }
+
+        //             }
+        //             catch (ClassCastException ex) {
+        //                 ex.printStackTrace(); // remove it later
+        //                 // System.exit(-1);
+        //             }
+        //         }
+        //     }
+        // }
     }
 }

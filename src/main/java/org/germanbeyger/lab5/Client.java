02963693dc6626f35a2c88d0ba5763f9f4ed5659
German Beiger <germaniga4@gmail.com>
2021-05-31 13:05:22 +0300

    another commit

--- src/main/java/org/germanbeyger/lab5/Client.java
+++ src/main/java/org/germanbeyger/lab5/Client.java
@@ -6,17 +6,14 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 import org.germanbeyger.lab5.commands.SendableCommand;
-import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
-        try ( Socket socket = new Socket(args[0], Integer.parseInt(args[1]));
-                Scanner stdInScanner = new Scanner(System.in)) {
+        try ( Socket socket = new Socket(args[0], Integer.parseInt(args[1]))) {
             System.out.println("Connected!");
             SendableCommand command = new SendableCommand(new String[]{"asdf", "sdaf"}, "add");
             ObjectOutputStream objStream = new ObjectOutputStream(socket.getOutputStream());
             objStream.writeObject(command);
-            System.out.println();
         } catch (UnknownHostException ex) {
             ex.printStackTrace();
         } 
